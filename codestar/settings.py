de019830b977b13419875acc16e6961132e4e813
Ladyofgoats <will12emery@googlemail.com>
2024-06-19 13:12:16 +0000

    tutor

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '.gitpod.io']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
